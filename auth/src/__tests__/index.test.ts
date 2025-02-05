084ccd04486334737a91cd6e6c154b5ec0ffa7fa
0xp0tato <ninadmaster05@gmail.com>
2025-02-05 17:19:28 +0530

    ADD : added test file

---
+++ auth/src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import { it } from "node:test";
+import request from "supertest";
+import { app } from "../app";
+
+it("TEST: Response of status 200", async () => {
+  await request(app).get("/").expect(200);
+});
